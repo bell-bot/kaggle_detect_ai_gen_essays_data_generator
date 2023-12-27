668cfa08b89ce7a2d1da12d802e138af05fa28d3
Bella <annabel.jakob@hotmail.de>
2023-12-27 23:51:52 +0000

    user can now change file locations via command line

---
+++ generate_essays.py
@@ -0,0 +1,44 @@
+from src.utils.generator_utils import generate_essays
+
+
+if __name__ == "__main__":
+    num_essays = int(input("Please enter the number of essays to generate:  "))
+    path_to_train_prompts = "data/train_prompts.csv"
+    path_to_training_essays = "data/training_essays.csv"
+    out_path = "data/generated_essays.csv"
+    print(f"""
+        The program currently expects the file containing the prompts and the file containing the training essays to 
+        be in the following locations:
+        - {path_to_train_prompts}
+        - {path_to_training_essays}
+    """)
+    change_location = input("Do you want to change the locations? (Y/n):    ")
+    if change_location.lower() == "y":
+        while True:
+            print("Please enter the location of the training prompts.")
+            path_to_train_prompts = input()
+
+            confirm = input(f"Please confirm that the new location is {path_to_train_prompts}. (Y/n):     ")
+            if confirm.lower() == "y":
+                break
+
+        while True:
+            print("Please enter the location of the training data")
+            path_to_training_essays = input()
+
+            confirm = input(f"Please confirm that the new location is {path_to_training_essays}. (Y/n):     ")
+            if confirm.lower() == "y":
+                break
+
+    change_out_location = input(f"The generated essays will be stored in {out_path}. Do you want to change this location? (Y/n):    ")
+
+    if (change_out_location.lower() == "y"):
+        while True:
+            print("Please enter the location and filename for the output file.")
+            out_path = input()
+
+            confirm = input(f"Please confirm that the new location is {out_path}. (Y/n):     ")
+            if confirm.lower() == "y":
+                break
+
+    generate_essays(path_to_train_prompts, path_to_training_essays, num_essays, out_path)
--- src/generate_essays.py
+++
@@ -1,5 +0,0 @@
-from src.utils.generator_utils import generate_essays
-
-if __name__ == "__main__":
-    num_essays = input("Please enter the number of essays to generate:  ")
-    generate_essays("data/train_prompts.csv","data/training_essays.csv",num_essays,"data/generated_essays.csv")
